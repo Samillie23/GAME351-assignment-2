28ba78addbe8440a7b008c90ad67e798c074de83
Samillie23 <anisferia2023@gmail.com>
2025-09-16 18:02:44 -0700

    Added hover script and physics based movement
    The "springs" on each of the corners make the car move up and down while it goes over the terrain. While the AddForce and AddTorque make the car move both forward and backwards. Does have some problems flipping when moving too fast though.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,35 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f; // Set player's movement speed.
-    public float rotationSpeed = 120.0f; // Set player's rotation speed.
-    private Rigidbody rb; // Reference to player's Rigidbody.
+    public float speed = 0; // Set player's movement speed.
+    public float rotationSpeed = 0; // Set player's rotation speed.
+
+    public Rigidbody rb; // Reference to player's Rigidbody.
+    public GameObject CM; // Center of mass for stable movement
+    public List<GameObject> Springs; // Makes the car hover
+    public float hoverForce = 0;
 
     // Start is called before the first frame update
     private void Start()
     {
-        rb = GetComponent<Rigidbody>(); // Access player's Rigidbody.
+        rb.centerOfMass = CM.transform.localPosition;
     }
 
     // Update is called once per frame
     void Update()
-    {  
+    {
+        
     }
 
     // Handle physics-based movement and rotation.
     private void FixedUpdate()
     {
-        // Move player based on vertical input.
-        float moveVertical = Input.GetAxis("Vertical");
-        Vector3 movement = transform.forward * moveVertical * speed * Time.fixedDeltaTime;
-        rb.MovePosition(rb.position + movement);
+        // Moving Forward
+        rb.AddForce(Time.deltaTime * transform.TransformDirection(Vector3.forward) * Input.GetAxis("Vertical") * (100f * speed));
 
-        // Rotate player based on horizontal input.
-        float turn = Input.GetAxis("Horizontal") * rotationSpeed * Time.fixedDeltaTime;
-        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
-        rb.MoveRotation(rb.rotation * turnRotation);
+        // Rotation
+        rb.AddTorque(Time.deltaTime * transform.TransformDirection(Vector3.up) * Input.GetAxis("Horizontal") * (100f * rotationSpeed));
+
+        // Hover code
+        foreach (GameObject spring in Springs)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(spring.transform.position, transform.TransformDirection(Vector3.down), out hit, 15f))
+            {
+                rb.AddForceAtPosition(Time.deltaTime * transform.TransformDirection(Vector3.up) * math.pow(15f - hit.distance, 2) / 15f * (100f * hoverForce), spring.transform.position);
+            }
+        }
+        rb.AddForce(-Time.deltaTime * transform.TransformDirection(Vector3.right) * transform.InverseTransformVector(rb.velocity).x * 5f);
     }
 }
